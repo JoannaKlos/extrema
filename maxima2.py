3ff263c8c697611e4f1dcda79050dac8b515c750
Asia Klos <asia@klos.com>
2019-02-15 11:11:34 +0100

    maxima after atom instalation

---
+++ maxima2.py
@@ -0,0 +1,9 @@
+def find_maxima(arr):
+  """Returns the positions of the macimum values in the array arr.
+
+  Example:
+  find_maxima ([1, 2, 1])
+  return [1, 2] → [1]
+  find_maxima([2, 0, 0, -2, 2]) → [0, 4]
+  """
+  return [0]
